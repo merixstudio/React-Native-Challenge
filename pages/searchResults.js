82b0155f50cc0746786af50928f8dde7c0d0376c
Michał Brzoza <mbrzoza@Michal-Brzoza.local>
2019-01-14 14:23:47 +0100

    search results clean up

--- pages/searchResults.js
+++ pages/searchResults.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Button, View, Text, FlatList } from "react-native";
-import { connect } from "react-redux";
-import { fetchBooks } from "../app/actions/bookActions";
-import { bindActionCreators } from "redux";
-import SearchForm from "../app/components/searchForm/searchForm";
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { Button, View, Text, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import { fetchBooks } from '../app/actions/bookActions';
+import SearchForm from '../app/components/searchForm/searchForm';
 import ListItem from '../app/components/listItem/listItem';
 
 export class SearchResultsScreen extends React.Component {
@@ -14,52 +14,59 @@ export class SearchResultsScreen extends React.Component {
     this.keyExtractor = this.keyExtractor.bind(this);
     this.onPressItem = this.onPressItem.bind(this);
   }
+
   componentDidMount() {
     const { navigation } = this.props;
-    const query = navigation.getParam("query", "NO-ID");
+    const query = navigation.getParam('query', 'NO-ID');
     this.fetchBooks(query);
   }
 
-  fetchBooks(query) {
-    this.props.actions.fetchBooks(query);
+  onPressItem(id) {
+    const { navigation } = this.props;
+    navigation.navigate('BookDetails', { id });
   }
 
-  keyExtractor(item, index) {
+  fetchBooks(query) {
+    const { actions } = this.props;
+    actions.fetchBooks(query);
+  }
+
+  keyExtractor(item) {
     return item.id;
   }
 
-  onPressItem (id)  {
-   this.props.navigation.navigate('BookDetails', {id});
-  };
-
-  renderItem = ({ item }) => (
-    <ListItem
-      id={item.id}
-      onPressItem={this.onPressItem}
-      title={item.volumeInfo.title}
-    />
-  );
+  renderItem({ item }) {
+    return (
+      <ListItem
+        id={ item.id }
+        onPressItem={ this.onPressItem }
+        title={ item.volumeInfo.title }
+      />
+    );
+  }
 
   render() {
-    const { isFetching, results } = this.props.books;
+    const { navigation, books } = this.props;
+    const { isFetching, results } = books;
+ 
     return isFetching || !results ? (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={ { flex: 1, alignItems: 'center', justifyContent: 'center' } }>
         <Text> Loading </Text>
       </View>
     ) : (
-      <View style={{ flex: 1, justifyContent: "center" }}>
+      <View style={ { flex: 1, justifyContent: 'center' } }>
         <Text>Search Results Screen</Text>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={ { flex: 1, flexDirection: 'row' } }>
           <Button
             title="Go to Home"
-            onPress={() => this.props.navigation.navigate("Home")}
+            onPress={ () => navigation.navigate('Home') }
           />
         </View>
-        <SearchForm onSubmit={values => this.fetchBooks(values.search)} />
+        <SearchForm onSubmit={values => this.fetchBooks(values.search) } />
         <FlatList
-          data={this.props.books.results.items}
-          keyExtractor={this.keyExtractor}
-          renderItem={this.renderItem}
+          data={ books.results.items }
+          keyExtractor={ this.keyExtractor }
+          renderItem={ this.renderItem }
         />
       </View>
     );
@@ -76,5 +83,5 @@ const mapDispatchToProps = dispatch => ({
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(SearchResultsScreen);
