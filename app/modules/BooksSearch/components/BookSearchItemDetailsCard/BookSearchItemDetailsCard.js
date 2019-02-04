fd343d3a09fe89d8529de2c731d680ece963bb22
Michał Brzoza <mbrzoza@Michal-Brzoza.local>
2019-02-04 01:03:24 +0100

    clean up

--- app/modules/BooksSearch/components/BookSearchItemDetailsCard/BookSearchItemDetailsCard.js
+++ app/modules/BooksSearch/components/BookSearchItemDetailsCard/BookSearchItemDetailsCard.js
@@ -16,91 +16,85 @@ import {
   Body,
 } from 'native-base';
 
-export class BookSearchItemDetailsCard extends React.Component {
-  render() {
-    const { current } = this.props;
-    return (
-      <Card>
-        <CardItem>
-          <Left>
-            <Thumbnail
-              square
-              source={ {
-                uri: current.volumeInfo.imageLinks
-                  ? current.volumeInfo.imageLinks.smallThumbnail
-                  : '/assets/icon.png',
-              } }
-            />
-            <Body>
-              <Text>
-                { current.volumeInfo.title }
-              </Text>
-              <Text note>
-                { current.volumeInfo.publishedDate }
-              </Text>
-            </Body>
-          </Left>
-        </CardItem>
-        {current.volumeInfo.authors && (
-          <CardItem>
-            <View style={ { flexDirection: 'row' } }>
-              <Text>
-                { current.volumeInfo.authors.length > 1 ? 'Authors:' : 'Author:'}
-                {' '}
-              </Text>
-              <FlatList
-                data={ current.volumeInfo.authors }
-                keyExtractor={ (item, index) => item[index] }
-                renderItem={ ({ item }) => <Text>{item}</Text> }
-              />
-            </View>
-          </CardItem>
-        )}
-        { current.volumeInfo.publisher && (
-          <CardItem>
-            <Text>
+const BookSearchItemDetailsCard = ({ current }) => (
+  <Card>
+    <CardItem>
+      <Left>
+        <Thumbnail
+          square
+          source={ {
+            uri: current.volumeInfo.imageLinks
+              ? current.volumeInfo.imageLinks.smallThumbnail
+              : '/assets/icon.png',
+          } }
+        />
+        <Body>
+          <Text>
+            { current.volumeInfo.title }
+          </Text>
+          <Text note>
+            { current.volumeInfo.publishedDate }
+          </Text>
+        </Body>
+      </Left>
+    </CardItem>
+    {current.volumeInfo.authors && (
+      <CardItem>
+        <View style={ { flexDirection: 'row' } }>
+          <Text>
+            { current.volumeInfo.authors.length > 1 ? 'Authors:' : 'Author:'}
+            {' '}
+          </Text>
+          <FlatList
+            data={ current.volumeInfo.authors }
+            keyExtractor={ (item, index) => item[index] }
+            renderItem={ ({ item }) => <Text>{item}</Text> }
+          />
+        </View>
+      </CardItem>
+    )}
+    { current.volumeInfo.publisher && (
+      <CardItem>
+        <Text>
                         Publisher:
-              {' '}
-              { current.volumeInfo.publisher }
-            </Text>
-          </CardItem>
-        )}
-        <CardItem>
-          <Body>
-            <Image
-              style={ {
-                height: 200, width: 200, flex: 1, marginBottom: 20,
-              } }
-              source={ {
-                uri: current.volumeInfo.imageLinks
-                  ? current.volumeInfo.imageLinks.thumbnail
-                  : '/assets/icon.png',
-              } }
-            />
+          {' '}
+          { current.volumeInfo.publisher }
+        </Text>
+      </CardItem>
+    )}
+    <CardItem>
+      <Body>
+        <Image
+          style={ {
+            height: 200, width: 200, flex: 1, marginBottom: 20,
+          } }
+          source={ {
+            uri: current.volumeInfo.imageLinks
+              ? current.volumeInfo.imageLinks.thumbnail
+              : '/assets/icon.png',
+          } }
+        />
+        <Text>
+          { current.volumeInfo.description ? current.volumeInfo.description.replace(/<\/?[^>]+(>|$)/g, '') : '' }
+        </Text>
+      </Body>
+    </CardItem>
+    { current.volumeInfo.averageRating && (
+      <CardItem>
+        <Left>
+          <Button transparent textStyle={ { color: '#87838B' } }>
+            <Icon name="logo-google" />
             <Text>
-              { current.volumeInfo.description ? current.volumeInfo.description.replace(/<\/?[^>]+(>|$)/g, '') : '' }
-            </Text>
-          </Body>
-        </CardItem>
-        { current.volumeInfo.averageRating && (
-          <CardItem>
-            <Left>
-              <Button transparent textStyle={ { color: '#87838B' } }>
-                <Icon name="logo-google" />
-                <Text>
                       Average Rating
-                  {' '}
-                  { current.volumeInfo.averageRating }
-                </Text>
-              </Button>
-            </Left>
-          </CardItem>
-        )}
-      </Card>
-    );
-  }
-}
-
+              {' '}
+              { current.volumeInfo.averageRating }
+            </Text>
+          </Button>
+        </Left>
+      </CardItem>
+    )}
+  </Card>
+);
 
 BookSearchItemDetailsCard.propTypes = {
   current: PropTypes.shape({
